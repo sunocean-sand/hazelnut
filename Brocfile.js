4a08e56b3b6f4d9de750d65725269c280d0001d7
Jessica H Lee <jessica.hyejin@gmail.com>
2015-06-08 01:30:33 -0700

    more configuration files and firebase

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', { destDir: 'assets' });
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
@@ -18,3 +22,4 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 module.exports = app.toTree();
+
