03912f11135949932b32d221a93ad9c7898fab60
Jessica H Lee <jessica.hyejin@gmail.com>
2015-06-11 13:48:52 -0700

    starting to take out legacy files

--- app/router.js
+++ app/router.js
@@ -6,15 +6,15 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
-  this.resource('lists', {path: '/'}, function() {
-    this.route('show', {path: 'todos/:list_id'} );
-  });
-  this.route('todo', {path: 'todos/:todo_id'});
-  this.route('details', {path: 'todos/:todo_id/details'});
+  //this.resource('lists', {path: '/'}, function() {
+  //  this.route('show', {path: 'todos/:list_id'} );
+  //});
+  //this.route('todo', {path: 'todos/:todo_id'});
+  //this.route('details', {path: 'todos/:todo_id/details'});
 
-  this.route('main', {path: 'stacks'});
-  this.route('honeybee', {path: 'stacks/:stack_id'});
-  this.route('lavender', {path: 'stacks/:stack_id/:task_id'});
-  this.route('manage', {path: 'stacks/:stack_id/manage'});
+  this.route('main', {path: '/'});
+  this.route('honeybee', {path: '/:stack_id'});
+  this.route('lavender', {path: '/:stack_id/:task_id'});
+  this.route('manage', {path: '/:stack_id/manage'});
   this.route('profile');
 });
