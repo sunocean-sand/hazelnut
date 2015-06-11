585d952acb5476300d49dbce619326667a9085d5
Jessica H Lee <jessica.hyejin@gmail.com>
2015-06-11 13:29:30 -0700

    comments are being saved and shown again!!!

--- app/routes/lavender.js
+++ app/routes/lavender.js
@@ -6,4 +6,34 @@ export default Ember.Route.extend({
 	model: function(params) {
 		return this.store.find('todo', params.task_id);
 	},
+
+	actions: {
+
+		createComment: function() {
+
+			var newComment = this.controllerFor(this.routeName).get('newComment');
+			var user = this.controllerFor('application').get('model');
+
+			if (Ember.isBlank(newComment)) {return false;}
+
+			var todo = this.modelFor(this.routeName);
+
+			var comment = this.store.createRecord('comment', {
+				message: newComment,
+				todo: todo,
+				user: user,
+			});
+
+			this.controllerFor(this.routeName).set('newComment', '');
+
+
+			comment.save().then(function(comment) {
+				todo.get('comment').addObject(comment);
+				todo.save();
+				user.get('comment').addObject(comment);
+				user.save();
+			});
+
+		},
+	}
 });
