8ce9a0cfe4544dcd1fd38d016e062e54184e93f7
Jessica H Lee <jessica.hyejin@gmail.com>
2015-06-18 17:04:08 -0700

    part of task details editing function

--- app/components/task-detail.js
+++ app/components/task-detail.js
@@ -2,4 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+	isEditing: false,
+
+	actions: {
+
+		editDetail: function() {
+			this.set('isEditing', true);
+		},
+
+		updateDetail: function() {
+
+			var store = this.get('store');
+			var model = this.get('todo');
+
+			if (Ember.isBlank(model.get('detail'))) {
+				model.rollback();
+			}
+			else {
+				model.save();
+				this.set('isEditing', false);
+			}
+		},
+	}
+	
 });
