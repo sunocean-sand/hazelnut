d8f84c903ed0b1cb8617ea220a626d960f014eec
Jessica H Lee <jessica.hyejin@gmail.com>
2015-06-23 16:53:00 -0700

    manage route works

--- app/routes/manage.js
+++ app/routes/manage.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params) {
-		return this.store.find('list', params.list_id);
+		return this.store.find('list', params.stack_id);
 	},
 });
