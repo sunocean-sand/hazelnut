1625ccc0d582420331ddded17c9e2079ff845e40
Jessica H Lee <jessica.hyejin@gmail.com>
2015-06-23 19:20:27 -0700

    added post buttons for adding tasks and comments for mobile usability

--- app/models/todo.js
+++ app/models/todo.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	title: DS.attr('string'),
 	isCompleted: DS.attr('boolean', {defaultValue: false}),
-	detail: DS.attr('string', {defaultValue: false}),
+	detail: DS.attr('string', {defaultValue: "add details.."}),
     idx:DS.attr('number', {defaultValue: false}),
 	timestamp: DS.attr('date'),
 
